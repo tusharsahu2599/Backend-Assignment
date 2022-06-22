10fef941350ffbc6902f9f6e06957bee53b66485
Tushar Sahu <56548569+tusharsahu2599@users.noreply.github.com>
2022-06-22 22:08:41 +0530

    flattened added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,13 +6,31 @@ const router = express.Router()
 router.get('/',async(req,res)=>{
 
     try{
-        const user = await User.find().lean().exec()
-        res.status(200).send(user)
+        const data = await User.find().lean().exec()
+        const flattenJSON = (obj = {}, res = {}, extraKey = '') => {
+            for(let key in obj){
+               if(typeof obj[key] !== 'object' && typeof obj[key] !== 'function'){
+                  res[extraKey + key] = typeof(obj[key])
+        
+               }
+               
+               else{
+                if(key === '_id' || key === '__v' || key === 'createdAt' || key === 'updatedAt' || key === 'dob'){
+                    res[extraKey + key] = 'number'
+                }
+                  flattenJSON(obj[key], res, `${extraKey}${key}.`);
+               };
+            };
+            return res;
+         };
+         res.send(flattenJSON(data));
+    }catch(err){
+        res.status(500).send(err)
     }
-    catch(err){
-        res.status(500).send(err.message)
-    }
-})
+}
+)
+         
+
 
 
 router.post("/",async(req,res)=>{
